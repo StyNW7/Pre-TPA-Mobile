4b59d0282f24b0a13481e30cf9671a554fc569e2
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2025-10-02 22:50:17 +0700

    Finish setuping

--- app/src/main/java/edu/bluejack25_1/pretpa/activities/MainActivity.kt
+++ app/src/main/java/edu/bluejack25_1/pretpa/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package edu.bluejack25_1.pretpa
+package edu.bluejack25_1.pretpa.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import edu.bluejack25_1.pretpa.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
